b4ac476ef610575b878996631413bfc2e5c2b93b
erwan <erwan.simon@epitech.eu>
2017-11-30 17:25:15 +0100

    first test for the subTable filling

--- Example.cs
+++ Example.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Security.Policy;
 
 class GomocupEngine : GomocupInterface
 {
 	const int MAX_BOARD = 100;
 	int[,] board = new int[MAX_BOARD, MAX_BOARD];
 	Random rand = new Random();
+	int[][] lines = new int[24][];
 
 	public override string brain_about
 	{
@@ -12,6 +14,42 @@ class GomocupEngine : GomocupInterface
 		{
 			return "name=\"Random\", author=\"Petr Lastovicka\", version=\"1.1\", country=\"Czech Republic\", www=\"http://petr.lastovicka.sweb.cz\"";
 		}
+	}
+
+	private void FillLines()
+	{
+		// lines
+		lines[0] = new[] {0, 0, 4, 0};
+		lines[1] = new[] {0, 1, 4, 1};
+		lines[2] = new[] {0, 2, 4, 2};
+		lines[3] = new[] {0, 3, 4, 3};
+		lines[4] = new[] {0, 4, 4, 4};
+		// collumns
+		lines[5] = new[] {0, 0, 0, 4};
+		lines[6] = new[] {1, 0, 1, 4};
+		lines[7] = new[] {2, 0, 2, 4};
+		lines[8] = new[] {3, 0, 3, 4};
+		lines[9] = new[] {4, 0, 4, 4};
+		// diags (top left -> bottom right)
+		lines[10] = new[] {4, 0, 4, 0}; // une seule case
+		lines[11] = new[] {3, 0, 4, 1};
+		lines[12] = new[] {2, 0, 4, 2};
+		lines[13] = new[] {1, 0, 4, 3};
+		lines[14] = new[] {0, 0, 4, 4};
+		lines[15] = new[] {0, 1, 3, 4};
+		lines[16] = new[] {0, 2, 2, 4};
+		lines[17] = new[] {0, 3, 1, 4};
+		lines[18] = new[] {0, 4, 0, 4}; // une seule case
+		// diags (top right -> bottom left)
+		lines[19] = new[] {0, 0, 0, 0}; // une seule case
+		lines[20] = new[] {0, 1, 1, 0};
+		lines[21] = new[] {0, 2, 2, 0};
+		lines[22] = new[] {0, 3, 3, 0};
+		lines[23] = new[] {0, 4, 4, 0};
+		lines[24] = new[] {1, 4, 4, 1};
+		lines[25] = new[] {2, 4, 4, 2};
+		lines[26] = new[] {3, 4, 4, 3};
+		lines[27] = new[] {4, 4, 4, 4}; // une seule case
 	}
 
 	public override void brain_init()
@@ -26,6 +64,7 @@ class GomocupEngine : GomocupInterface
 			Console.WriteLine("ERROR Maximal board size is " + MAX_BOARD);
 			return;
 		}
+		FillLines();
 		Console.WriteLine("OK");
 	}
 
@@ -47,7 +86,7 @@ class GomocupEngine : GomocupInterface
 	{
 		if (isFree(x, y))
 		{
-			board[x, y] = 1;
+			board[x, y] = -2;
 		}
 		else
 		{
@@ -59,7 +98,7 @@ class GomocupEngine : GomocupInterface
 	{
 		if (isFree(x, y))
 		{
-			board[x, y] = 2;
+			board[x, y] = -1;
 		}
 		else
 		{
@@ -71,7 +110,7 @@ class GomocupEngine : GomocupInterface
 	{
 		if (isFree(x, y))
 		{
-			board[x, y] = 3;
+			board[x, y] = -3;
 		}
 		else
 		{
@@ -116,6 +155,11 @@ class GomocupEngine : GomocupInterface
 
     // ALGO
 
+	void Algo()
+	{
+		
+	}
+
     int[,] ReturnSubTable(int id)
     {
         int x = (id % (width - 4)) - 2;
@@ -135,5 +179,138 @@ class GomocupEngine : GomocupInterface
         return res;
     }
 
+	void MakeSquare(int square, ref int enNb, ref int alNb, ref int emNb)
+	{
+		switch (square)
+		{
+			case 1:
+				alNb += 1;
+				emNb = (enNb == 0 ? emNb : 0);
+				enNb = 0;
+				break;
+			case -1:
+				enNb += 1;
+				emNb = (alNb == 0 ? emNb : 0);
+				alNb = 0;
+				break;
+			default:
+				emNb += 1;
+				break;
+		}		
+	}
 
+	void SetBack(ref int[,] subTable, int[] line, ref int enNb, ref int alNb, ref int emNb)
+	{
+		int y = line[1];
+		int x = line[0];
+		while (y != line[3] || x != line[2])
+		{
+			if (subTable[x, y] == (enNb != 0 ? -1 : -2))
+				break;
+			x += (line[0] < line[2] ? 1 : (line[0] > line[2] ? -1 : 0));
+			y += (line[1] < line[3] ? 1 : (line[1] > line[3] ? -1 : 0));			
+		}
+		while (y != line[3] || x != line[2])
+		{
+			if (subTable[x, y] != -1 && subTable[x, y] != -2)
+				subTable[x, y] += (enNb != 0 ? enNb : alNb) * (enNb != 0 ? enNb : alNb) + (enNb != 0 ? 0 : 1);
+			x += (line[0] < line[2] ? 1 : (line[0] > line[2] ? -1 : 0));
+			y += (line[1] < line[3] ? 1 : (line[1] > line[3] ? -1 : 0));
+		}
+		emNb = 0;
+	}
+
+	void FillSubTable(int[,] subTable)
+	{
+		foreach (int[] line in lines)
+		{
+			int y = line[1];
+			int x = line[0];
+			int alNb = 0;
+			int enNb = 0;
+			int emNb = 0;
+			while (y != line[3] || x != line[2])
+			{
+				switch (subTable[x, y])
+				{
+					case -1:
+						if (alNb != 0 && emNb != 0)
+							SetBack(ref subTable, line, ref alNb, ref enNb, ref emNb);
+						enNb += 1;
+						alNb = 0;
+						break;
+					case -2:
+						if (enNb != 0 && emNb != 0)
+							SetBack(ref subTable, line, ref alNb, ref enNb, ref emNb);
+						alNb += 1;
+						enNb = 0;
+						break;
+					case -3:
+						break;
+					default:
+						emNb += 1;
+						break;
+				}
+				x += (line[0] < line[2] ? 1 : (line[0] > line[2] ? -1 : 0));
+				y += (line[1] < line[3] ? 1 : (line[1] > line[3] ? -1 : 0));
+			}
+			if (emNb != 0)
+				SetBack(ref subTable, line, ref alNb, ref enNb, ref emNb);
+		}
+	}
+
+/*
+	void FillSubTable(int[,] subTable)
+	{
+		int x;
+		int y;
+		int enNb = 0; // enemy number
+		int alNb = 0; // ally number
+		int emNb = 0; // empty number
+		
+		for (y = 0; y != 5; y += 1)
+		{
+			for (x = 0; x != 5; x += 1)
+			{
+				MakeSquare(subTable[x, y], ref enNb, ref alNb, ref emNb);
+			}
+			for (x = 0; x != 5; x += 1)
+			{
+				if (subTable[x, y] == 0 || (subTable[x, y] != 1 && subTable[x, y] != -1))
+					subTable[x, y] += (alNb + enNb + (emNb / 2)) * (alNb + enNb + (emNb / 2)) + (alNb > 0 ? 1 : 0);
+			}
+		}
+		for (x = 0; x != 5; x += 1)
+		{
+			for (y = 0; y != 5; y += 1)
+			{
+				MakeSquare(subTable[x, y], ref enNb, ref alNb, ref emNb);
+			}
+			for (y = 0; y != 5; y += 1)
+			{
+				if (subTable[x, y] == 0 || (subTable[x, y] != 1 && subTable[x, y] != -1))
+					subTable[x, y] += (alNb + enNb + (emNb / 2)) * (alNb + enNb + (emNb / 2)) + (alNb > 0 ? 1 : 0);
+			}
+		}
+		alNb = 0;
+		enNb = 0;
+		emNb = 0;
+		y = 0;
+		x = 0;
+		for (int k = 0 ; k < 5 * 2 ; k++ )
+		{
+			for (y = 0 ; y <= k ; y++ )
+			{
+				x = k - y;
+				if( x < 5 && y < 5 ) {
+					MakeSquare(subTable[x, y], ref enNb, ref alNb, ref emNb);
+				}
+			}
+		}
+	}
+*/
+	void SetResBoard()
+	{
+		
+	}
 }
